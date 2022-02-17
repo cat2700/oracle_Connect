9c8da871f38bb1ec2fcb139739f4e40c5851e163
HomePC <cat2700@gmail.com>
2022-02-17 20:22:04 +0200

    commit

--- prog1.py
+++ prog1.py
@@ -2,7 +2,7 @@ import conn
 
 
 cn = conn.my_conn(uid='arabank', upsw='icl', service_name="oracl2k")
-print(cn.open_connect())
+# print(cn.open_connect())
 # print(cn.close_connect())
 # rs = list(cn.runSQL("select * from temp_dep"))
 # rs = list(cn.runSQL("insert into TEMP values ('2','حامد محمد')"))
@@ -17,4 +17,5 @@ print(cn.open_connect())
 # for o in rs[2]:
 #     print(o[:2])
 
-cn.backupORRestore(isrestore=True, restoreFile='bk')
+cn.backupORRestore(
+    isrestore=True, restoreFile='all-16-02-2022')
